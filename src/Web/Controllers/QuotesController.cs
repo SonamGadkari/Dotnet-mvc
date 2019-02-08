cb0c18369c33012bc50d4990196c63efb0c3eac5
Sonam Gadekari <S533490@nwmissouri.edu>
2019-02-07 23:22:31 -0600

    commit to Controllers/QuotesController.cs

---
+++ src/Web/Controllers/QuotesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace mvc2.Controllers
+{
+    public class QuotesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShowText()
+        {
+             return Content("Returns the text or Content");
+        }
+        
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public JsonResult Get()
+        {
+            return Json(_authorRepository.List());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
