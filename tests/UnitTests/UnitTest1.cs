f552ac504a7a86744d1aadddf48cda1cc75ce5a7
Sonam Gadekari <S533490@nwmissouri.edu>
2019-02-08 15:18:47 -0600

    Test case added for compound interest

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -23,11 +23,27 @@ namespace UnitTests
             var actual1 = SimpleInterest(principle,time,rate);
             Assert.Equal(expected1, actual1);
         }
-
+        
         double SimpleInterest(float principle, float time,double interest)
         {
             return (principle * time * interest / 100);
+        }    
+
+        public void CompoundInterest_float()
+        {
+            var expected1 = 10.2;
+            var principle =200;
+            var time = 2;
+            var rate= 2.3;
+            var actual1 = CompoundInterest(principle,time,rate);
+            Assert.Equal(expected1, actual1);
+
         }
+        
+        double CompoundInterest(float principle, float time,double interest)
+        {
+            return ((1+(principle * time * interest / 100));
+        }  
 
     }
 }
